b5612575add0dea777fba148e57a01bdf77de1c8
cdp055 <116390098+cdp055@users.noreply.github.com>
2022-11-16 19:39:48 -0500

    Add files via upload

---
+++ src/hipotenusa.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class hipotenusa {
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        System.out.print("Digite cateto 1: ");
+        float cateto1=entrada.nextFloat();
+        System.out.print("Digite cateto 2: ");
+        float cateto2=entrada.nextFloat();
+        double hipotenusa=Math.sqrt(Math.pow(cateto1,2)+Math.pow(cateto2,2));
+        System.out.println("La hipotenusa tiene un valor de: "+hipotenusa);
+    }
+}
